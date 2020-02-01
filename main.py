ec23edfd3f18b9b1f061c2d3f192f8ecc5ccddfd
Kerooker <leonardo.colman98@gmail.com>
2020-01-31 22:36:40 -0300

    Adiciona comando de contagem

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from telegram import Update, Bot
 from telegram.ext import Dispatcher, CommandHandler
+from datetime import datetime, time
 
 
 def intercomp_callback(bot, update, **optional_args):
@@ -11,10 +12,30 @@ def intercomp_callback(bot, update, **optional_args):
     update.message.reply_text("ÔÔÔÔÔÔ", quote=False)
 
 
+def contagem_callback(bot, update, **optional_args):
+    def dateDiffInSeconds(date1, date2):
+        timedelta = date2 - date1
+
+        return timedelta.days * 24 * 3600 + timedelta.seconds
+
+    def daysHoursMinutesSecondsFromSeconds(seconds):
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+        return days, hours, minutes, seconds
+
+    data_intercomp = datetime.strptime('2020-06-11 00:00:01', '%Y-%m-%d %H:%M:%S')
+    now = datetime.now()
+
+    update.message.reply_text("FALTAM %d dias, %d horas, %d minutos e %d segundos pro INTERCOMP NINJA EDITION"
+                              % daysHoursMinutesSecondsFromSeconds(dateDiffInSeconds(now, data_intercomp)))
+
+
 def webhook(request):
     bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
     dispatcher = Dispatcher(bot, None, 0)
     dispatcher.add_handler(CommandHandler("intercomp", intercomp_callback))
+    dispatcher.add_handler(CommandHandler("contagem", contagem_callback))
 
     if request.method == "POST":
         update = Update.de_json(request.get_json(force=True), bot)
